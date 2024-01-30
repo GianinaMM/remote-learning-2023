1e4d29da0a5f4c33340d0d5f013eefa572effef6
Gianina Morosan <morosan.gianina1@gmail.com>
2024-01-30 16:38:53 +0200

    JAvaScript

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+console.log("hello");
+
+var name = "Paul";
+
+let age = 12;
+
+const contition = true;
+
+//nu pot incepe cu numar,pot incepe cu $ _ sau pot contine
+
+
+console.log(name);
+
+function doSomething() {
+    console.log("Do something!!");
+
+}
+
+doSomething();
+
+
+function add(a, b) {
+    return a + b
+}
+
+console.log(add(1, 2));
+
+
+if(conditie) {
+    console.log("Este true")
+}else {
+    console.log("Este false")
+}
